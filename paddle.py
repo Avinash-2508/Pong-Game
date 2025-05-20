302755f8f47dd2c84e44e2877b79ed55312b5fab
Avinash <avinash.ponneboina@gmail.com>
2025-05-20 22:34:41 +0530

    Change x position

--- paddle.py
+++ paddle.py
@@ -9,7 +9,7 @@ class Paddle(Turtle):
         self.goto(position)
 
     def go_up(self):
-        new_y = self.ycor() +20
+        new_y = self.ycor() +2
         self.goto(self.xcor(), new_y)
     def go_down(self):
         new_y = self.ycor() - 20
