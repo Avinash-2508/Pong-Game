a5ab4799c7ae8830626af203c61060d5a6fb8f11
Avinash <avinash.ponneboina@gmail.com>
2025-06-20 23:15:49 +0530

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,7 +1,7 @@
 from turtle import Turtle, Screen
 class Ball(Turtle):
     def __init__(self):
-        super().__init__(
+        super().__init__()
         self.color("white")
         self.shape("circle")
         self.penup()
