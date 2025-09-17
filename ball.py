e302ebfce871fb3dcdcb9b44f6c1b8885b5573fd
Avinash <avinash.ponneboina@gmail.com>
2025-09-17 23:38:07 +0530

    test triangle

--- ball.py
+++ ball.py
@@ -3,7 +3,7 @@ class Ball(Turtle):
     def __init__(self):
         super().__init__()
         self.color("white")
-        self.shape("circl")
+        self.shape("circle")
         self.penup()
         self.x_move = 10
         self.y_move = 10
@@ -27,6 +27,7 @@ class Ball(Turtle):
         self.move_speed = 0.1
         self.bounce_x()
         
+
 
 
 
